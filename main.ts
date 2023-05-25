17f18ab3b1c50498147f1a6e04e37727f7313445
jinjoisu <133915914+jinjoisu@users.noreply.github.com>
2023-05-25 13:25:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -23,8 +23,8 @@ kitronik_air_quality.show("Data logging, ready!", 2, kitronik_air_quality.ShowAl
 basic.showIcon(IconNames.Yes)
 basic.pause(2000)
 kitronik_air_quality.clear()
-kitronik_air_quality.setDate(24, 5, 23)
-kitronik_air_quality.setTime(11, 37, 0)
+kitronik_air_quality.setDate(25, 5, 23)
+kitronik_air_quality.setTime(1, 25, 0)
 kitronik_air_quality.addProjectInfo("JJ", "IAQ")
 kitronik_air_quality.selectSeparator(kitronik_air_quality.Separator.tab)
 kitronik_air_quality.includeDate()
@@ -33,16 +33,16 @@ kitronik_air_quality.includeTemperature(kitronik_air_quality.TemperatureUnitList
 kitronik_air_quality.includeHumidity()
 kitronik_air_quality.includeIAQ()
 counter = 0
-loops.everyInterval(60000, function () {
-    kitronik_air_quality.measureData()
-    kitronik_air_quality.logData()
-    counter += 1
-    kitronik_air_quality.show("Data points:" + counter, 7, kitronik_air_quality.ShowAlign.Left)
-})
 basic.forever(function () {
     kitronik_air_quality.show(kitronik_air_quality.readDate(), 1, kitronik_air_quality.ShowAlign.Right)
     kitronik_air_quality.show(kitronik_air_quality.readTime(), 1, kitronik_air_quality.ShowAlign.Left)
     kitronik_air_quality.show("Temp:" + kitronik_air_quality.readTemperature(kitronik_air_quality.TemperatureUnitList.F), 3, kitronik_air_quality.ShowAlign.Left)
     kitronik_air_quality.show("R.H.:" + kitronik_air_quality.readHumidity(), 4, kitronik_air_quality.ShowAlign.Left)
     kitronik_air_quality.show("IAQ:" + kitronik_air_quality.getAirQualityScore(), 5, kitronik_air_quality.ShowAlign.Left)
+})
+loops.everyInterval(3600000, function () {
+    kitronik_air_quality.measureData()
+    kitronik_air_quality.logData()
+    counter += 1
+    kitronik_air_quality.show("Data points:" + counter, 7, kitronik_air_quality.ShowAlign.Left)
 })
